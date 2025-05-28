f02d7220e3b0f8b8037f119ed5f2eb53151dad35
chaidhat <chaimongkol@ucla.edu>
2025-05-28 02:42:01 -0700

    change

--- website/src/components/pages/HomePage.tsx
+++ website/src/components/pages/HomePage.tsx
@@ -93,7 +93,7 @@ export default function HomePage() {
             <div className="relative w-full aspect-video">
               <iframe
                 className="absolute top-0 left-0 w-full h-full rounded-lg"
-                src="https://youtu.be/pl1HtK5VVbQ"
+                src="https://www.youtube.com/embed/pl1HtK5VVbQ"
                 title="OpenSesame Demo"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
